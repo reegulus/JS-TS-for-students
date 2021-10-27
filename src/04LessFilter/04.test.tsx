26f03c22e217973ccaaaeb7e4278bd3f1f3f87ea
Egorych <mikhalevichegor@gmail.com>
2021-10-27 16:47:48 +0300

    "get only completed tasks"

--- src/04LessFilter/04.test.tsx
+++ src/04LessFilter/04.test.tsx
@@ -11,7 +11,6 @@ test("should take old men older then 90", () => {
     expect(oldAges.length).toBe(1)
     expect(oldAges[0]).toBe(100)
 })
-
 test("should take chip courses 160", () => {
 
     const chipCourses = courses.filter(chipPredicate)
@@ -20,4 +19,21 @@ test("should take chip courses 160", () => {
     expect(chipCourses.length).toBe(2)
     expect(chipCourses[0].title).toBe("CSS")
     expect(chipCourses[1].title).toBe("HTML")
+})
+
+
+test("get only completed tasks", () => {
+    const tasks= [
+        {id: 1, title: "bread", isDone: false},
+        {id: 2, title: "milk", isDone: true},
+        {id: 3, title: "cookies", isDone: false},
+        {id: 4, title: "sol", isDone: false},
+        {id: 5, title: "chocolate", isDone: true},
+    ]
+    const completedTasks =tasks.filter(task => task.isDone)
+
+    expect(completedTasks.length).toBe(2)
+    expect(completedTasks[0].id).toBe(2)
+    expect(completedTasks[1].id).toBe(5)
+
 })
