7249ce604cea823053c8bf6e55e406ba91ad197c
Egorych <mikhalevichegor@gmail.com>
2021-10-27 16:23:15 +0300

    04/ test("should take chip courses 160"

--- src/04LessFilter/04.test.tsx
+++ src/04LessFilter/04.test.tsx
@@ -1,21 +1,23 @@
+import {ages, chipPredicate, courses, predicate} from "./04";
+
 test("should take old men older then 90", () => {
 
-    const predicate = (age: number) => {
+    /*const predicate = (age: number) => {
         return age>90
-    }
+    }*/
+    // const predicate = (age: number) => age>90
     const ages = [18,20, 22, 1, 100, 14]
-      const oldAges = ages.filter(predicate )
+    const oldAges = ages.filter(predicate)
     expect(oldAges.length).toBe(1)
     expect(oldAges[0]).toBe(100)
 })
 
-test("should take ", () => {
+test("should take chip courses 160", () => {
 
-    const predicate = (age: number) => {
-        return age>90
-    }
-    const ages = [18,20, 22, 1, 100, 14]
-      const oldAges = ages.filter(predicate )
-    expect(oldAges.length).toBe(1)
-    expect(oldAges[0]).toBe(100)
+    const chipCourses = courses.filter(chipPredicate)
+
+
+    expect(chipCourses.length).toBe(2)
+    expect(chipCourses[0].title).toBe("CSS")
+    expect(chipCourses[1].title).toBe("HTML")
 })
--- src/04LessFilter/04.tsx
+++ src/04LessFilter/04.tsx
@@ -1,6 +1,6 @@
-const ages = [18,20, 22, 1, 100, 14]
+export const ages = [18,20, 22, 1, 100, 14]
 
-const predicate = (age: number) => {
+export const predicate = (age: number) => {
     return age>90
 }
 const oldAges = [100]
@@ -9,17 +9,17 @@ type CourseType = {
     price: number
 }
 
-const courses = [
+export const courses = [
     {title: "CSS", price: 110},
     {title: "JS", price: 210},
     {title: "HTML", price: 150}
 ]
 
-const chipPredicate = (courses: CourseType) => {
+export const chipPredicate = (courses: CourseType) => {
     return courses.price < 160
 }
 
-const chipCourses = [
+export const chipCourses = [
     {title: "CSS", price: 110},
     {title: "HTML", price: 150},
 ]
