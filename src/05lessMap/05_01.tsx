88914b3152018aa08b3b30129dd0019ec6867f60
Egorych <mikhalevichegor@gmail.com>
2021-10-31 20:49:27 +0300

    05 - JS/TS - map

---
+++ src/05lessMap/05_01.tsx
@@ -0,0 +1,11 @@
+export type ManType = {
+    name: string
+    age: number
+}
+
+
+const people: Array<ManType> = [
+    {name: "Andrew Ivanov", age: 25},
+    {name: "Alexander Petrov", age: 15},
+    {name: "Dmitry Sidorov", age: 35}
+]
